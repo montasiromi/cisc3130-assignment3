ef7398891913483762000da12eb4d17d1fbebbff
katychuang <katychuang@gmail.com>
2018-11-04 23:33:21 -0500

    Queen shaped output

--- maven-example/src/main/java/cisc3130/App.java
+++ maven-example/src/main/java/cisc3130/App.java
@@ -4,11 +4,11 @@ import com.kennycason.kumo.nlp.FrequencyAnalyzer;
 import com.kennycason.kumo.palette.ColorPalette;
 import com.kennycason.kumo.bg.Background;
 import com.kennycason.kumo.bg.RectangleBackground;
+import com.kennycason.kumo.bg.PixelBoundryBackground;
 import com.kennycason.kumo.CollisionMode;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
-
 import java.awt.*;
 import java.io.*;
 import java.util.*;
@@ -20,13 +20,19 @@ import java.util.*;
 public class App {
     public static void main( String[] args ) throws FileNotFoundException, java.io.IOException  {
         final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        final java.util.List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("output.txt");
-        final Dimension dimension = new Dimension(600, 600);
-        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.RECTANGLE);
-        wordCloud.setPadding(0);
-        wordCloud.setBackground(new RectangleBackground(dimension));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
-        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+        frequencyAnalyzer.setWordFrequenciesToReturn(300);
+        frequencyAnalyzer.setMinWordLength(2);
+
+
+        final java.util.List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("Bohemian_Rhapsody.txt");
+
+        final Dimension dimension = new Dimension(600, 200);
+        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+        wordCloud.setPadding(2);
+        wordCloud.setBackground(new PixelBoundryBackground("queen.png"));
+        wordCloud.setBackgroundColor(new Color(0x0000F0FF, true));
+        wordCloud.setFontScalar(new LinearFontScalar(4, 10));
+
         wordCloud.build(wordFrequencies);
         wordCloud.writeToFile("wordcloud_rectangle.png");
     }
